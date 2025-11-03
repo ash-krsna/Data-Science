3a80f031c6f7180718be18d974c805f51a2cc8a8
Akash Ohalkar <akashohalkar300@gmail.com>
2025-11-03 17:10:24 +0530

    Initial commit - phase1 notebook added

---
+++ numpy_pandas_basics.py
@@ -0,0 +1,14 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# NumPy test
+arr = np.array([1, 2, 3, 4, 5])
+print("NumPy array:", arr)
+print("Mean value:", np.mean(arr))
+
+# Pandas test
+data = {"Name": ["Akash", "Ravi"], "Age": [21, 23]}
+df = pd.DataFrame(data)
+print("\nPandas DataFrame:")
+print(df)
